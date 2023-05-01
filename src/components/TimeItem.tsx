9bb3d5abbb6ef8e78208d6b6a56ff3edb6586e35
johnsn27 <nathan.johnson@bbc.co.uk>
2023-05-01 12:51:34 +0100

    T11 - Times that are in the past or within 2 hours of current time is disabled, disabled class added to TimeItem

--- src/components/TimeItem.tsx
+++ src/components/TimeItem.tsx
@@ -1,4 +1,5 @@
 import RootStore from '../RootStore';
+import moment from 'moment';
 import { observer } from "mobx-react-lite";
 import FormatTime from '../helpers/formatTime';
 
@@ -8,7 +9,7 @@ interface Props {
 }
 
 const TimeItem = observer(({ time, rootStore }: Props) => {
-  const { selectedTime, setSelectedTime } = rootStore;
+  const { selectedTime, setSelectedTime, selectedDay } = rootStore;
 
   const handleClick = () => {
     setSelectedTime(time);
@@ -16,11 +17,24 @@ const TimeItem = observer(({ time, rootStore }: Props) => {
 
   const formattedTime = FormatTime(time)
 
+  const isDisabled = () => {
+    if (!selectedDay) {
+      return true;
+    }
+
+    const now = moment();
+    const selectedDateTime = moment(selectedDay, "MMM Do YYYY").add(time, "hours");
+    const twoHoursFromNow = moment().add(2, "hours");
+
+    return now.isSameOrAfter(selectedDateTime) || selectedDateTime.isBefore(twoHoursFromNow);
+  };
+
 
     return (
       <button
-        className={`timeItem ${selectedTime === time ? "selected" : ""}`}
+        className={`timeItem ${selectedTime === time ? "selected" : ""} ${isDisabled() ? "disabled" : ""}`}
         onClick={handleClick}
+        disabled={isDisabled()}
       >
         {formattedTime}
       </button>
