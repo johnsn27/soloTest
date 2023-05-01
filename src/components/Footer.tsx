60b730d6c6e1c2e8c0c56afca828d5732fe566f5
johnsn27 <nathan.johnson@bbc.co.uk>
2023-05-01 13:15:51 +0100

    T13 - display the number of pros available for the selected day in the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,32 @@
 import { useStore } from "../App";
 import { observer } from "mobx-react-lite";
+import { useState, useEffect } from "react";
+import mockApi from "../mockApi";
 
 const Footer = observer(() => {
   const rootStore = useStore();
+  const [numberOfPros, setNumberOfPros] = useState<number | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleBookNow = () => {
     rootStore.requestBooking();
   };
 
-  const isTimeSelected = !!rootStore.selectedTime; // check if a time is selected
+  const isTimeSelected = !!rootStore.selectedTime;
+
+  useEffect(() => {
+    const fetchPros = async () => {
+      if (rootStore.selectedDay) {
+        setIsLoading(true);
+        const dayOfMonth = parseInt(rootStore.selectedDay.split(" ")[2]);
+        const numberOfPros = await mockApi.getNumberOfPros(dayOfMonth);
+        setNumberOfPros(numberOfPros);
+        setIsLoading(false);
+      }
+    };
+
+    fetchPros();
+  }, [rootStore.selectedDay]);
 
   return (
     <div className="footer">
@@ -16,12 +34,20 @@ const Footer = observer(() => {
         <p>
         <b>{rootStore.selectedDateTime}</b>
         </p>
-        <p>0 professionals available</p>
+        {isLoading ? (
+          <p>...</p>
+        ) : numberOfPros !== null ? (
+          <p>
+            {numberOfPros} professional{numberOfPros !== 1 && "s"} available
+          </p>
+        ) : (
+          <p>Please select a day</p>
+        )}
       </div>
       <button
         className="bookButton"
         onClick={handleBookNow}
-        disabled={!isTimeSelected}
+        disabled={!isTimeSelected || numberOfPros === 0 || isLoading}
       >
         Book Now
       </button>
