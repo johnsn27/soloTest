a30d7375ac7f88acc5d631af699350ffe060389c
johnsn27 <nathan.johnson@bbc.co.uk>
2023-05-01 11:35:40 +0100

    T2 - Format the TimeItem text

--- src/components/TimeItem.tsx
+++ src/components/TimeItem.tsx
@@ -1,9 +1,21 @@
+import moment from 'moment';
+
 interface Props {
   time: number;
 }
 
 const TimeItem = ({ time }: Props) => {
-  return <button className="timeItem">{time}</button>;
+
+  const hours = Math.floor(time);
+  const minutes = (time - hours) * 60;
+
+  const formattedTime = moment()
+    .hour(hours)
+    .minute(minutes)
+    .format('h:mm A');
+
+
+  return <button className="timeItem">{formattedTime}</button>;
 };
 
 export default TimeItem;
